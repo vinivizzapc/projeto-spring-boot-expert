fcb101831ecbea414251b8518bce4c76cf07de51
vinivizzapc <vini_vizza@hotmail.com>
2024-02-18 22:31:57 -0300

    Implementando Documentação da API com swagger

--- src/main/java/io/github/vvcampos/api/controller/ClienteController.java
+++ src/main/java/io/github/vvcampos/api/controller/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.vvcampos.api.controller;
 
 import io.github.vvcampos.domain.entity.Cliente;
 import io.github.vvcampos.service.ClienteService;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api Clientes")
 public class ClienteController {
 
     private ClienteService service;
@@ -19,12 +21,22 @@ public class ClienteController {
     }
 
     @GetMapping("{id}")
-    public Cliente getById( @PathVariable Integer id ){
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado para o ID informado")
+    })
+    public Cliente getClienteById( @PathVariable @ApiParam("Id do cliente") Integer id ){
       return service.getClienteById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva um novo cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 404, message = "Erro de validação")
+    })
     public Cliente save( @RequestBody @Valid Cliente cliente ) {
         return service.save(cliente);
     }
